02fd4231da5d6c7db158f56718c773e0f3de2044
Chris Ortman <chris-ortman@uiowa.edu>
2018-07-06 11:37:53 -0500

    This formatter does not work with syslog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,6 @@ Rails.application.configure do
   if ENV['RAILS_LOG_TO_SYSLOG'].present? && ENV['RAILS_LOG_TO_SYSLOG'] == 'true'
     require 'syslog/logger'
     logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'sparc-fulfillment')
-    logger.formatter = ::Logger::Formatter.new
     config.logger = logger
   end
 
