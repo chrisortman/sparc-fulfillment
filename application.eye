26dad01008ed8d69cf959b1f135bc0f4eec613c1
Chris Ortman <chris-ortman@uiowa.edu>
2019-09-03 16:21:45 -0500

    Hab build plan upgrades
    - faster build
    - leverage build & runtime environment settings
    - local eye & .env

---
+++ application.eye
@@ -0,0 +1,112 @@
+RAILS_ROOT = ENV["RAILS_ROOT"] || File.expand_path(File.dirname(__FILE__))
+RAILS_ENV  = ENV["RAILS_ENV"] || "development"
+RAILS_PORT = ENV["RAILS_PORT"] || "4000"
+FAYE_PORT  = ENV["FAYE_PORT"] || "9292"
+USE_SYSLOG = ENV['RAILS_LOG_TO_SYSLOG'].present?
+
+if USE_SYSLOG
+  require 'syslog/logger'
+end
+
+WORKERS_COUNT=1
+
+def puma_log
+  if USE_SYSLOG
+    #Syslog::Logger.new('sparc-request')
+    ":syslog"
+  else
+    "log/puma.log"
+  end
+end
+
+def dj_log(name)
+  if USE_SYSLOG
+    #Syslog::Logger.new('sparc-request-jobs')
+    ":syslog"
+  else
+    "log/#{name}.log"
+  end
+end
+
+def faye_log
+  if USE_SYSLOG
+    ":syslog"
+  else
+    "log/fulfillment_faye.stdall.log"
+  end
+end
+
+def delayed_job_process(proxy, name)
+  rails_env = proxy.env['RAILS_ENV']
+  proxy.process(name) do
+    start_command "bin/delayed_job -e #{rails_env} run"
+    pid_file "tmp/pids/#{name}.pid"
+    stdall dj_log(name)
+    daemonize true
+    stop_signals [:INT, 30.seconds, :TERM, 10.seconds, :KILL]
+    check :cpu, every: 30, below: 80, times: 3
+    check :memory, every: 30, below: 300.megabytes, times: 5
+  end
+end
+
+Eye.config do
+  if USE_SYSLOG
+    logger syslog
+  else
+    logger "#{RAILS_ROOT}/log/eye.log"
+  end
+end
+
+Eye.application 'fulfillment' do
+  working_dir RAILS_ROOT
+  env ({
+    "PORT" => RAILS_PORT 
+  })
+  load_env ".env"
+#  env 'APP_ENV' => 'production' # global env for each processes
+  trigger :flapping, times: 10, within: 1.minute, retry_in: 10.minutes
+
+  group 'web' do
+
+    process :puma do
+      daemonize true
+      stdall puma_log
+      pid_file "tmp/pids/puma.pid" # pid_path will be expanded with the working_dir
+      start_command "bin/puma -p #{RAILS_PORT} -e #{RAILS_ENV}"
+      stop_signals [:TERM, 5.seconds, :KILL]
+      restart_command 'kill -USR2 {PID}'
+      # when no stop_command or stop_signals, default stop is [:TERM, 0.5, :KILL]
+      # default `restart` command is `stop; start`
+
+
+      # ensure the CPU is below 30% at least 3 out of the last 5 times checked
+      check :cpu, every: 30, below: 80, times: 3
+    end
+
+    process :faye do
+      opts = [
+        "-p #{FAYE_PORT}",
+        "-P tmp/pids/fulfillment_faye_thin.pid",
+        '-d',
+        '-R faye.ru',
+        "--tag fulfillment_faye_thin",
+        '-t 30',
+        "-e #{RAILS_ENV}",
+        "-c #{RAILS_ROOT}",
+        '-a 127.0.0.1'
+      ]
+      pid_file "tmp/pids/fulfillment_faye_thin.pid"
+      start_command "bin/thin start #{opts * ' '}"
+      stop_signals [:QUIT, 2.seconds, :TERM, 1.seconds, :KILL]
+      stdall faye_log
+
+    end
+  end
+
+  group 'jobs' do
+    chain grace: 5.seconds
+    (1..WORKERS_COUNT).each do |i|
+      delayed_job_process self, "dj-#{i}"
+    end
+  end
+end
--- habitat/plan.sh
+++ habitat/plan.sh
@@ -4,9 +4,14 @@
 pkg_name=sparc-fulfillment
 pkg_origin=chrisortman
 pkg_version="2.8.5"
-pkg_source="https://github.com/ui-icts/sparc-fulfillment/archive/${pkg_name}-${pkg_version}.tar.bz2"
-# Overwritten later because we compute it based on the repo
-pkg_shasum="TODO"
+ruby_pkg="core/ruby24"
+ruby_major="2.4.0"
+
+# Have to do this because we are creating the package from our git repo
+pkg_filename=${pkg_name}-${pkg_version}.tar.gz
+
+app_sub_path="static/release"
+
 pkg_deps=(
   core/libxml2
   core/libxslt
@@ -19,8 +24,7 @@ pkg_deps=(
   core/rsync
   core/zlib
 
-  core/ruby24
-  chrisortman/eye
+  $ruby_pkg
   )
 pkg_build_deps=(
   core/coreutils
@@ -29,6 +33,8 @@ pkg_build_deps=(
   core/make
   core/which
   core/cacerts
+  core/tar
+  core/yarn
 )
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
@@ -46,40 +52,35 @@ pkg_exports=(
 # Callback Functions
 #
 do_begin() {
+  # Because we don't have pkg_source set
+  # we need to explicitly set a source path
+  # potentially it could all work without it
+  # but then we'd be changing stuff in our
+  # same directory as where we have our git
+  # repository and I don't want to accidentally
+  # rm -fr something
+  # If you delete this you're going to wind
+  # up in $PLAN_CONTEXT in the do_build func
+  SRC_PATH=$CACHE_PATH
   return 0
 }
 
 do_download() {
-  export GIT_SSL_CAINFO="$(pkg_path_for core/cacerts)/ssl/certs/cacert.pem"
-
-  # This is a way of getting the git code that I found in the chef plan
-  build_line "Fake download! Creating archive of latest repository commit from $PLAN_CONTEXT"
-  cd $PLAN_CONTEXT/..
-  git archive --prefix=${pkg_name}-${pkg_version}/ --output=$HAB_CACHE_SRC_PATH/${pkg_filename} HEAD
-
-  pkg_shasum=$(trim $(sha256sum $HAB_CACHE_SRC_PATH/${pkg_filename} | cut -d " " -f 1))
+  return 0
 }
 
 do_verify() {
-  do_default_verify
-  # return 0
-}
-
-# The default implementation removes the HAB_CACHE_SRC_PATH/$pkg_dirname folder
-# in case there was a previously-built version of your package installed on
-# disk. This ensures you start with a clean build environment.
-do_clean() {
-  do_default_clean
+  return 0
 }
 
 do_unpack() {
-  do_default_unpack
-  # return 0
+  build_line "Cloning source files"
+  cd $PLAN_CONTEXT/../
+  { git ls-files; git ls-files --exclude-standard --others; } \
+    | _tar_pipe_app_cp_to $HAB_CACHE_SRC_PATH/${pkg_dirname}
 }
 
 do_prepare() {
-
-  export BUNDLE_SILENCE_ROOT_WARNING=1
 
   build_line "Setting link for /usr/bin/env to 'coreutils'"
   [[ ! -f /usr/bin/env ]] && ln -s "$(pkg_path_for coreutils)/bin/env" /usr/bin/env
@@ -87,20 +88,46 @@ do_prepare() {
   return 0
 }
 
-do_build() {
+do_setup_environment() {
 
-  export CPPFLAGS="${CPPFLAGS} ${CFLAGS}"
+  set_runtime_env TZ "America/Chicago"
+  set_runtime_env RAILS_ROOT "${pkg_prefix}/${app_sub_path}"
+
+  push_runtime_env GEM_PATH "${pkg_prefix}/vendor/bundle/ruby/${ruby_major}"
+  set_runtime_env LD_LIBRARY_PATH "$(pkg_path_for "core/gcc-libs")/lib:$(pkg_path_for "core/libevent")"
+  set_runtime_env HOME ${pkg_svc_var_path}/home
+
+  set_runtime_env RAILS_ENV "production"
+  set_runtime_env RACK_ENV "production"
+  set_runtime_env LANG "en_US.UTF-8"
+  set_runtime_env LC_ALL "en_US.UTF-8"
+  set_runtime_env EYE_HOME "${pkg_svc_var_path}/eye"
+
+  set_buildtime_env HOME /root
+  mkdir --parents '/hab/cache/artifacts/studio_cache/yarn'
+  set_buildtime_env YARN_CACHE_FOLDER '/hab/cache/artifacts/studio_cache/yarn'
+
+  set_buildtime_env BUNDLE_SILENCE_ROOT_WARNING 1
+
+  set_buildtime_env MY_BUNDLE_CACHE_PATH $HAB_CACHE_SRC_PATH/bundle_cache
+  set_buildtime_env MY_TMP_CACHE_PATH $HAB_CACHE_SRC_PATH/tmp_cache
+  set_buildtime_env MY_NODE_CACHE_PATH $HAB_CACHE_SRC_PATH/node_cache
+
   local _libxml2_dir=$(pkg_path_for libxml2)
   local _libxslt_dir=$(pkg_path_for libxslt)
   local _zlib_dir=$(pkg_path_for zlib)
+  local _mysql2_dir=$(pkg_path_for mysql-client)
   local _openssl_include_dir=$(pkg_path_for openssl)
 
   # don't let bundler split up the nokogiri config string (it breaks
   # the build), so specify it as an env var instead
-  export NOKOGIRI_CONFIG="--use-system-libraries --with-zlib-dir=${_zlib_dir} --with-xslt-dir=${_libxslt_dir} --with-xml2-include=${_libxml2_dir}/include/libxml2 --with-xml2-lib=${_libxml2_dir}/lib"
+  set_buildtime_env NOKOGIRI_CONFIG "--use-system-libraries --with-zlib-dir=${_zlib_dir} --with-xslt-dir=${_libxslt_dir} --with-xml2-include=${_libxml2_dir}/include/libxml2 --with-xml2-lib=${_libxml2_dir}/lib"
 
-  export EVENTMACHINE_CONFIG="--with-ssl-dir=${_openssl_include_dir}"
-  # we control the variable above, and it will be all on one line, and
+  set_buildtime_env MYSQL_CONFIG "--with-mysql-dir=${_mysql2_dir}"
+  set_buildtime_env EVENTMACHINE_CONFIG "--with-ssl-dir=${_openssl_include_dir}"
+}
+do_build() {
+
   # we need single quotes otherwise the extconf doesn't build the
   # extension.
   bundle config build.nokogiri '${NOKOGIRI_CONFIG}'
@@ -119,29 +146,40 @@ do_build() {
    fi
 
    ####### ZOOM ######
-   # If you want to speed up your habitat package build
-   # while you're dev'ing do this after your first run
-   # in the studio
-   # cp -a /hab/cache/src/sparc-fulfillment-$pkg_version/vendor/bundle /hab/cache/src/bundle_cache
-   # but you'll probably have to put the pkg version in there
-   if [[ -e $HAB_CACHE_SRC_PATH/bundle_cache ]]; then
-     echo "Restoring cached bundle install"
-     cp -a $HAB_CACHE_SRC_PATH/bundle_cache vendor/bundle
+
+   if [[ -e $MY_BUNDLE_CACHE_PATH ]]; then
+     build_line "Restoring cached bundle gems"
+     mkdir -p vendor
+     cp -a $MY_BUNDLE_CACHE_PATH vendor/bundle
    fi
 
-  bundle install --without test development deploy --jobs 2 --retry 5 --path vendor/bundle --no-binstubs
+   if [[ -e $MY_NODE_CACHE_PATH ]]; then
+     build_line "Restoring cached node modules"
+     rm -fr node_modules
+     cp -a $MY_NODE_CACHE_PATH node_modules
+   fi
 
-  # cp -R vendor/bundle $HAB_CACHE_SRC_PATH/bundle_cache
-  # Some bundle files when they install have permissions that don't
-  # allow the all user to read them, but because we are running as
-  # root right now for building, but as 'hab' or someone else when the
+   if [[ -e $MY_TMP_CACHE_PATH ]]; then
+     build_line "Restoring cached assets"
+     mkdir -p tmp
+     cp -a $MY_TMP_CACHE_PATH tmp/cache
+   fi
+
+   build_line "Bundle install gems"
+   bundle install \
+     --path vendor/bundle \
+     --without test:development \
+     --retry 5 \
+     --binstubs \
+     --quiet
+
   # package installs we need to make sure we can read the files
   chmod -R a+rx vendor/bundle
 
   # Need to generate a database.yml if there isn't one
   if [[ ! -e config/database.yml ]]; then
     clean_up_db=true
-    echo "Creating stub database.yml"
+    build_line "Creating stub database.yml"
     cat << NULLDB > config/database.yml
 production:
   adapter: nulldb
@@ -150,28 +188,28 @@ NULLDB
   fi
 
   if [[ ! -e config/sparc_db.yml ]]; then
-    echo "Copying default sparc_db.yml for asset compilation"
+    build_line "Copying default sparc_db.yml for asset compilation"
     cp config/sparc_db.yml.example config/sparc_db.yml
     sed -e "s#development#production#" -i "config/sparc_db.yml"
   fi
 
   if [[ ! -e config/faye.yml ]]; then
-    echo "Copying default faye.yml for asset compilation"
+    build_line "Copying default faye.yml for asset compilation"
     cp config/faye.yml.example config/faye.yml
     sed -e "s#development#production#" -i "config/faye.yml"
   fi
 
   if [[ ! -e .env ]]; then
-    echo "Creating stub .env"
+    build_line "Creating stub .env"
     cat << EOF > .env
 GLOBAL_SCHEME=http
 SPARC_API_HOST=localhost:3000
 SPARC_API_VERSION=v1
-SPARC_API_USERNAME=javais
-SPARC_API_PASSWORD=betterthanruby
+SPARC_API_USERNAME=
+SPARC_API_PASSWORD=
 APPLICATION_TIME_ZONE=America/Chicago
 FAYE_REFRESH_INTERVAL=25
-FAYE_TOKEN=3bd16424a2154769a1fb4adc081b10747bdbfdbd93682032
+FAYE_TOKEN=
 CWF_FAYE_HOST=localhost:9292
 DOCUMENTS_FOLDER=documents
 DOCUMENT_ROOT=documents
@@ -184,14 +222,16 @@ LDAP_BASE='dc=iowa,dc=uiowa,dc=edu'
 LDAP_ENCRYPTION=simple_tls
 LDAP_USER_DOMAIN=@uiowa.edu
 # for LDAP service accounts
-LDAP_AUTH_USERNAME='icts-nebula-svc'
-LDAP_AUTH_PASSWORD='n-@bh:Ai3#8x8'
+LDAP_AUTH_USERNAME=''
+LDAP_AUTH_PASSWORD=''
 USE_EPIC=false
 SECRET_KEY_BASE=6e0367f990fa7854b1bf9592656989b26ac59e7b586d260383a7460448cbfd77dc00c8c5a83d03b9a37277e5fc3908d1d1ad86ac097b3f2ec4f150cecb4bef74
 EOF
 
   fi
-  RAILS_ENV=production bin/rake assets:precompile
+
+  build_line "Precompiling assets"
+  RAILS_ENV=production bin/rake -s assets:precompile
 
   rm config/sparc_db.yml
   rm config/faye.yml
@@ -232,20 +272,20 @@ do_install() {
   # that is all versioned out.
   # EDIT: I changed the cp -r to cp -a cuz maybe that's better
   # since I'm setting my user to hab up above anyway?
-  echo "Copying current files to ${pkg_prefix}"
-  mkdir -p "${pkg_prefix}/static/release"
-  cp -a . "${pkg_prefix}/static/release"
+  build_line "Copying current files to ${pkg_prefix}"
+  mkdir -p "$RAILS_ROOT"
+  cp -a . "$RAILS_ROOT"
 
 
   # This seems to be some habitat stuff that you 
   # just need to do?
-  for binstub in ${pkg_prefix}/static/release/bin/*; do
+  for binstub in $RAILS_ROOT/bin/*; do
     build_line "Setting shebang for ${binstub} to 'ruby'"
-    [[ -f $binstub ]] && sed -e "s#/usr/bin/env ruby#$(pkg_path_for ruby24)/bin/ruby#" -i "$binstub"
+    [[ -f $binstub ]] && sed -e "s#/usr/bin/env ruby#$(pkg_path_for ${ruby_pkg})/bin/ruby#" -i "$binstub"
   done
-  for binstub in ${pkg_prefix}/static/release/script/*; do
+  for binstub in $RAILS_ROOT/script/*; do
     build_line "Setting shebang for ${binstub} to 'ruby'"
-    [[ -f $binstub ]] && sed -e "s#/usr/bin/env ruby#$(pkg_path_for ruby24)/bin/ruby#" -i "$binstub"
+    [[ -f $binstub ]] && sed -e "s#/usr/bin/env ruby#$(pkg_path_for ${ruby_pkg})/bin/ruby#" -i "$binstub"
   done
 
   if [[ $(readlink /usr/bin/env) = "$(pkg_path_for coreutils)/bin/env" ]]; then
@@ -253,11 +293,14 @@ do_install() {
     rm /usr/bin/env
   fi
 
-  chmod +x ${pkg_prefix}/static/release/script/upgrade/*.sh
-  create_symlinks
+  chmod +x $RAILS_ROOT/script/upgrade/*.sh
+  _create_symlinks
+  _create_process_bin "${pkg_prefix}/bin/rake" rake
+  _create_process_bin "${pkg_prefix}/bin/rails" rails
+  _create_process_bin "${pkg_prefix}/bin/eye" eye
 }
 
-create_symlinks() {
+_create_symlinks() {
   rm -rfv ${pkg_prefix}/static/release/log
   rm -rfv ${pkg_prefix}/static/release/tmp
   rm -rfv ${pkg_prefix}/static/release/documents
@@ -277,6 +320,7 @@ create_symlinks() {
   ln -sfv ${pkg_svc_config_path}/sparc_db.yml ${pkg_prefix}/static/release/config/sparc_db.yml
   ln -sfv ${pkg_svc_config_path}/appenv ${pkg_prefix}/static/release/.env
 }
+
 # The default implementation is to strip any binaries in $pkg_prefix of their
 # debugging symbols. You should override this behavior if you want to change
 # how the binaries are stripped, which additional binaries located in
@@ -284,6 +328,102 @@ create_symlinks() {
 # binaries stripped at all.
 do_strip() {
   return 0
+}
+
+do_after() {
+  if [[ $HAB_CREATE_PACKAGE == 'false' ]]; then
+    build_line "WARN: Skipping artifact creation because 'HAB_CREATE_PACKAGE=false'"
+
+    _generate_artifact() {
+      return 0
+    }
+
+    _prepare_build_outputs() {
+      return 0
+    }
+  fi
+}
+# There is no default implementation of this callback. This is called after the
+# package has been built and installed. You can use this callback to remove any
+# temporary files or perform other post-install clean-up actions.
+do_end() {
+  if [[ "$STORE_CACHES" == "true" ]]; then
+    build_line "Caching expensive dependencies"
+    
+    # Remove first so that our cp command behaves consistently
+    rm -frv $MY_BUNDLE_CACHE_PATH
+    rm -frv $MY_NODE_CACHE_PATH
+    rm -frv $MY_TMP_CACHE_PATH
+
+    cp -a $HAB_CACHE_SRC_PATH/${pkg_dirname}/vendor/bundle $MY_BUNDLE_CACHE_PATH
+    [[ -d $HAB_CACHE_SRC_PATH/${pkg_dirname}/node_modules ]] && cp -a $HAB_CACHE_SRC_PATH/${pkg_dirname}/node_modules $MY_NODE_CACHE_PATH
+    cp -a $HAB_CACHE_SRC_PATH/${pkg_dirname}/tmp/cache $MY_TMP_CACHE_PATH
+  fi
+
+  return 0
+}
+
+# **Internal** Use a "tar pipe" to copy the app source into a destination
+# directory. This function reads from `stdin` for its file/directory manifest
+# where each entry is on its own line ending in a newline. Several filters and
+# changes are made via this copy strategy:
+#
+# * All user and group ids are mapped to root/0
+# * No extended attributes are copied
+# * Some file editor backup files are skipped
+# * Some version control-related directories are skipped
+# * Any `./habitat/` directory is skipped
+# * Any `./vendor/bundle` directory is skipped as it may have native gems
+_tar_pipe_app_cp_to() {
+  local dst_path tar
+  dst_path="$1"
+  tar="$(pkg_path_for tar)/bin/tar"
+
+  mkdir -p $dst_path
+
+  "$tar" -cp \
+      --owner=root:0 \
+      --group=root:0 \
+      --no-xattrs \
+      --exclude-backups \
+      --exclude-vcs \
+      --exclude='habitat' \
+      --exclude='node_modules' \
+      --exclude='vendor/bundle' \
+      --exclude='results' \
+      --files-from=- \
+      -f - \
+  | "$tar" -x \
+      -C "$dst_path" \
+      -f -
+}
+
+_create_process_bin() {
+  local bin cmd env_sh
+  bin="$1"
+  cmd="$2"
+  env_sh="$pkg_svc_config_path/dotenv"
+
+  build_line "Creating ${bin} process bin"
+
+  cat <<EOF > "$bin"
+#!$(pkg_path_for busybox-static)/bin/sh
+set -e
+if test -n "\$DEBUG"; then set -x; fi
+export HOME="$pkg_svc_data_path"
+if [ -f "$env_sh" ]; then
+  source "$env_sh"
+else
+  >&2 echo "No dotenv file found: '$env_sh'"
+  >&2 echo "Have you not started this service ($pkg_origin/$pkg_name) before?"
+  >&2 echo ""
+  >&2 echo "Aborting..."
+  exit 1
+fi
+cd $RAILS_ROOT
+exec ./bin/$cmd \$@
+EOF
+  chmod -v 755 "$bin"
 }
 
 # There is no default implementation of this callback. This is called after the
